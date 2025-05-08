1070ce62ba6a2f86d6ba2eb2153cad661ebe7198
renehuanca <renehuanca999@gmail.com>
2025-05-08 12:04:47 -0400

    add audioplayers in pubspec

---
+++ lib/screens/sonido_animal_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+class SonidoAnimalScreen extends StatefulWidget {
+  const SonidoAnimalScreen({super.key});
+
+  @override
+  State<SonidoAnimalScreen> createState() => _SonidoAnimalScreenState();
+}
+
+class _SonidoAnimalScreenState extends State<SonidoAnimalScreen> {
+  TextEditingController controlText = TextEditingController();
+  final AudioPlayer _player = AudioPlayer();
+  String? animalActual; 
+  Map<String, Map<String, String>> datosAnimal = {
+    'gato': {
+      'image': 'assets/images/gato.png',
+      'sound': 'assets/sounds/gato.mp3',
+    },
+    'perro': {
+      'image': 'assets/images/perro.png',
+      'sound': 'assets/sounds/perro.mp3',
+    },
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sonido de animales'),
+      ),
+      body: Column(
+        children: [
+          Text('Escribe el nomre del animal gato o perro'),
+          TextField(
+            controller: controlText,
+          ),
+          ElevatedButton(onPressed: buscarAnimal, child: Text('Buscar')),
+          //image
+          SizedBox(height: 30,),
+          if (animalActual != null) ... {
+            Image.asset(datosAnimal[animalActual]!['image']!),
+            Text('es un: $animalActual'),
+          },
+        ],
+      ),
+    );
+  }
+
+  void buscarAnimal() {
+    String nombreAnimal = controlText.text;
+    if (datosAnimal.containsKey(nombreAnimal)) {
+      _player.play(AssetSource(datosAnimal[nombreAnimal]!['sound']!)); // viene con problemas probar sin esto
+      // ! = va haber algo que se va reproducir
+      setState(() {
+        animalActual = nombreAnimal;
+      });
+    }
+  }
+}
